092d8985f9207b71f4aee65a7ec4e4fb5f8a58ae
Hajdú Patrik Zsolt <87029162+hajdu-patrik@users.noreply.github.com>
2024-12-08 22:16:42 +0100

    Update DropdownItem.ts

--- components/Navbar/handheld/DropdownItem.ts
+++ components/Navbar/handheld/DropdownItem.ts
@@ -2,17 +2,17 @@ export const Hrefs = [
   {
     id: 1,
     name: "Rottenpaper",
-    href: "https://comingsoon.io",
+    href: "https://tyoh.io/rottenpaper",
   },
   {
     id: 2,
     name: "Twitter",
-    href: "https://twitter.com/theyardofhorror",
+    href: "https://twitter.com/tyoh",
   },
   {
     id: 3,
     name: "Discord",
-    href: "http://discord.gg/V26W8nrWcb",
+    href: "http://discord.gg/tyoh",
   },
   // {
   //   id: 4,
